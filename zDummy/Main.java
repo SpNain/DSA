87702ee653a51794a93c3c1ba4d0f4ff9b81b118
HardeepNain <hnspnain@gmail.com>
2023-06-03 18:47:46 +0530

    up

--- zDummy/Main.java
+++ zDummy/Main.java
@@ -4,8 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int x = 0;
+        int n = 10;
 
+        // // Edge cases
+        // if(n == 0 || n == 1 || n == 2){
+        //     System.out.println(0);
+        // }
+        // else if(n == 3){
+        //     System.out.println(1);
+        // }
+
+        int countPrimes = 0;
+        for (int num = 2; num < n; num++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j * j <= num; j++) {
+
+                if (num % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime == true) // agr num prime h to countPrimes ko increase krdo
+                countPrimes++;
+        }
         
+        System.out.println(countPrimes);
+
+
     }
 }
